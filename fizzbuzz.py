59064417b8259b9c0539538ff88dc64579bfeee6
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-05-19 18:44:04 +0000

    Second commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,23 +1,40 @@
 import sys 
 
-try:
-   numCount = int(sys.argv[1])
-except IndexError:
-    numCount = int(input("Well you need to put some digits  "))
-except NameError:
-    numCount = int(input("C'mon now we need a number"))
-except ValueError:
-    numCount = int(input("Tsk, not a letter, a number  "))   
+numCount = None
+
+while not numCount:
+    try:
+        numCount = int(sys.argv[1])
+    except IndexError:
+        numCount = int(input("Well you need to put some digits  "))
+    except NameError:
+        numCount = int(input("C'mon now we need a number"))
+    except ValueError:
+        try:
+            numCount = int(input("Tsk, not a letter, a number  "))  
+        except:
+            continue
+
+def fizzBuzz(n):
+    
+    for n in range (1, n + 1):
+        if n % 3 == 0 and n % 5 == 0:
+            print ("Fizzbuzz") 
+        elif n % 3 == 0:
+            print ("Fizz") 
+        elif n % 5 == 0: 
+            print ("Buzz") 
+        else:
+            print (n)
+
+fizzBuzz(numCount)
+fizzBuzz(10)
 
 
-for numCount in range (1, numCount + 1):
-    if numCount % 3 == 0 and numCount % 5 == 0:
-        print ("Fizzbuzz") 
-    elif numCount % 3 == 0:
-        print ("Fizz") 
-    elif numCount % 5 == 0: 
-        print ("Buzz") 
-    else:
-        print (numCount)
+
+
+
+
+
 
 
