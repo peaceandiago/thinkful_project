5662bc97b66fefcf778c2a9458d730bf1683e3ed
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-11-05 17:23:24 +0000

    modified bicycle

---
+++ bicycle.py
@@ -0,0 +1,26 @@
+class Bicycle(object): #spec
+    def __init__(name, weight, cost) 
+        self.name = name
+        self.weight = weight
+        self.cost = cost
+
+class BikeShop(object):
+    def __init__(name, inventory, margin, profit)
+        self.name = name
+        self.inventory = inventory
+        self.margin = margin
+        self.profit = profit
+    
+class Customer(object):
+    def __init__(name, fund, buy)
+        self.name = name
+        self.fund = fund
+        self.buy = buy
+
+fashionable = Bicycle("green", 15, 450)
+
+shop1_inventory = [fashionable]
+
+
+shop1 = BikeShop("Bathurst", shop1_inventory , 20%, 3000)
+
