d36a6aa04cc41bc31431a28db2e514ad2f7a82e5
peaceandiago <paola.santiago@mail.utoronto.ca>
2016-11-08 01:16:31 +0000

    modified the bicycle

--- bicycle.py
+++ bicycle.py
@@ -1,26 +1,32 @@
 class Bicycle(object): #spec
-    def __init__(name, weight, cost) 
+    def __init__(self, name, weight, cost): 
         self.name = name
         self.weight = weight
         self.cost = cost
 
 class BikeShop(object):
-    def __init__(name, inventory, margin, profit)
+    def __init__(self, name, inventory, margin, profit):
         self.name = name
         self.inventory = inventory
         self.margin = margin
         self.profit = profit
     
 class Customer(object):
-    def __init__(name, fund, buy)
+    def __init__(self, name, fund):
         self.name = name
         self.fund = fund
-        self.buy = buy
 
-fashionable = Bicycle("green", 15, 450)
+fashionable = Bicycle("high-society", 15, 450)
+road = Bicycle("road-trip", 25, 750)
+cyclocross = Bicycle("crosscycle", 30, 500)
+touring = Bicycle("tourlefrance", 28, 800)
+triathlon = Bicycle("seriousspeed", 20, 1000)
+flat = Bicycle("flatbard", 18, 750)
 
-shop1_inventory = [fashionable]
+shop1_inventory = [fashionable, road, cyclocross, touring, triathlon, flat]
+shop1 = BikeShop("Bathurst", shop1_inventory, 20, 3000)
+customer1 = Customer("Andrea", 450)
+customer2 = Customer("Beatrice", 1000)
+customer3 = Customer("Sylvia", 850)
 
-
-shop1 = BikeShop("Bathurst", shop1_inventory , 20%, 3000)
 
